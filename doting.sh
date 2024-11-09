c425ed66edafad7922d8976fba6c6c5ac9017cc1
WECLEY <wecwhengithub7.anything491@slmail.me>
2024-11-09 02:35:44 +0100

    added option ad and error handlings

--- doting.sh
+++ doting.sh
@@ -1,64 +1,87 @@
 #!/bin/bash
+# set -e
 
-if [ -z "$1" ];then
-    echo "Missing argument: bash doting.sh [path to file/dir]"
-    exit
+if [ -z "$1" ] || [ -z "$2" ] ;then
+	echo "Missing argument: bash doting.sh [path to file/dir]"
+	exit 0
 fi
 
 LINKER="archive.txt"
-
-DOTFILE_PATH=	#path to dotfile folder
-TOSYM_PATH=$1	#the argument that is the target file to be doted
-TOSYM_NAME=$(basename $1)
-
 SYM_OPT='-s' 	#linking option -s -sf
-SYM_TAR=
-SYM_LOC=
 
-# INITIALISING FUNCTIONS
+# ===[ INITIALISING FUNCTIONS ]=== #
+
+erex(){ #check and exit if error, avoid pouring fuel on the fire
+	# echo "nn hh $1" >> /dev/tty
+	if [[ $1 -ne 0 ]]; then
+		echo "error." >> /dev/tty
+		exit $1
+	else
+		echo "Done."  >> /dev/tty
+
+	fi
+}
 
 sym(){
+	# echo "ln $SYM_OPT $SYM_TAR $SYM_LOC" >> /dev/tty
 	ln $SYM_OPT $SYM_TAR $SYM_LOC #linking command
+	erex
+}
+
+update()
+{
+	echo "	updating $LINKER" 
+
+	printf "\n[ $TOSYM_NAME ]\n" >> $LINKER
+	printf "current=	$DOTFILE_PATH/$TOSYM_NAME\n" >> $LINKER
+	printf "previous=	$TOSYM_PATH" >> $LINKER
 }
 
 unarchive(){
 	LINE_A=$(awk "/ $1 / {printf NR}" $LINKER)
 	LINE_B=$((LINE_A + 3))
 	sed -i "$LINE_A , $LINE_B d" $LINKER
+
+	erex
 }
 
 ask(){
-    read -p "create and continue? (y/n):" INP
-    case $INP in
-        'y')
-            creatin
-            ;;
-        'n')
-            echo "abort."
-            exit
-            ;;
-        *)
-            echo "not an option, retry"
-            echo $INP
-            ask
-            ;;
-    esac
+	read -p "create and continue? (y/n):" INP
+	case $INP in
+		'y')creatin;;
+		
+		'n')echo "abort."
+			exit;;
+		
+		*)	echo "not an option, retry"
+			echo $INP
+			ask;;
+	esac
+
+}
+
+isexist(){
+	if [ ! -f "$1" ];then
+		echo "No file found."
+		exit 2
+	fi
 }
 
 creatin(){
-    echo "creatig installer"
-    touch $LINKER  
-    printf "\n#this file is automatically created, and saves paths for each file\n#used to undo a linking or redo it\n\n" >> $LINKER
+	echo "creatig installer"
+	touch $LINKER  
+	printf "\n#this file is automatically created, and saves paths for each file\n#used to undo a linking or redo it\n\n" >> $LINKER
 
-    # printf "if [ \$1 = 'f' ]; then\n\tSYM_OPT='-sf'\nfi\n\n" >> $LINKER
-    # printf "#The linking commands containing each doted's original path\n\n" >> $LINKER
-    # printf "if [ \$0 -neq 0 ]; then\n\t" >> $LINKER
-    # printf "echo \"Symlinks updated.\"\nfi\n" >> $LINKER
+	erex
 }
 
 doting()
 {
-	DOTFILE_PATH=$(pwd)
+	isexist
+	DOTFILE_PATH=$(pwd)	#path to dotfile folder
+	TOSYM_PATH=$1	#the argument that is the target file to be doted
+	TOSYM_NAME=$(basename $1) #file name extraction
+
 	printf "DOTFILE PATH: set to current location : $DOTFILE_PATH \n\n"
 
 	echo "	moving		'$TOSYM_NAME'	to	$DOTFILE_PATH/$TOSYM_NAME"
@@ -71,84 +94,84 @@ doting()
 	SYM_LOC=$TOSYM_PATH
 	#CALLING COMMAND
 	sym
+	update
+
+	erex
 }
 
-update()
-{
-	echo "	updating $LINKER" 
-
-	printf "\n[ $TOSYM_NAME ]\n" >> $LINKER
-	printf "current=	$DOTFILE_PATH/$TOSYM_NAME\n" >> $LINKER
-	printf "previous=	$TOSYM_PATH" >> $LINKER
-}
 
 resymlink()
 {
-	CURRENT=$(grep -A2 "$1" $LINKER | awk -v FS='\t' 'NR==2 {print $2}')
-	PREVIOUS=$(grep -A2 "$1" $LINKER | awk -v FS='\t' 'NR==3 {print $2}')
+	set -e #i dont wanna set erex for every rm and mv
 
+	isexist $1
 	echo "reinstalling symlink for $1"
+
+	CURRENT=$(getpath-archive $1 2)
+	PREVIOUS=$(getpath-archive $1 3)
 
 	#INITIALISING SYMLINKING COMMAND ARGUMENT
 	SYM_TAR=$CURRENT
 	SYM_LOC=$PREVIOUS
 	SYM_OPT='-sf'
 	#CALLING COMMAND
-	sym
+	sym || exit $?
+	}
+
+getpath-archive()
+{
+	# set -e
+	grep -A2 " $1 " $LINKER >> /dev/null #test command incase of errors, for set -e
+	# echo "nn hh $?" >> /dev/tty
+	erex $? || exit $?
+
+	#second arg: (1 file name, 2 current, 3 previous)
+	PATHH=$(\
+		grep -A2 " $1 " $LINKER |\
+		awk -v FS='\t' "NR==$2 {print \$2}"
+	)
+	echo "$PATHH"	
+
 }
 
 undo()
 {
-	if [ ! -f "$1" ];then
-		echo "File doesnt exist."
-		exit
-	fi
+	set -e #i dont wanna set erex for every rm and mv
 
-	CURRENT=$(grep -A2 "$1" $LINKER | awk -v FS='\t' 'NR==2 {print $2}')
-	PREVIOUS=$(grep -A2 "$1" $LINKER | awk -v FS='\t' 'NR==3 {print $2}')
+	isexist $1
 
 	echo "undoting $1 from dotfile"
+
+	PREVIOUS=$(getpath-archive "$1" 3)
+	CURRENT=$(getpath-archive "$1" 2)
 
 	rm "$PREVIOUS"
 	mv "$CURRENT" "$PREVIOUS"
 
-	unarchive $1
-
-	echo "done."
+	unarchive $1 || exit $?
 }
 
-erex(){
-if [ $? -gt 0 ]; then
-	echo "error."
-	exit 
-else
-	echo "Done."
-fi
-}
-# APPLICATION ====================================
 
-case $1 in
-	're')
-		resymlink $2
-		exit
-		;;
-	'un')
-		undo $2
-		exit
-		;;
-
-esac
+# ===[ APPLICATION  ]=== #
 
 if [ ! -f $LINKER ]; then #CREAT ARCHIVE
-    echo "no installer found. "
-    ask
+	echo "no installer found. "
+	ask
 fi
 
-doting
-erex
-update
-erex
+case $1 in
+	're')resymlink $2
+		exit
+		;;
+	'un')undo $2
+		exit
+		;;
+	'ad')doting $2
+		exit
+		;;
+	*)	echo "Unavailible argument."
+		exit
+		;;
+esac
 
-# SYM_OPT='$SYM_OPT'
-# SYM_CMD="ln $SYM_OPT $DOTFILE_PATH/$TOSYM_NAME $TOSYM_PATH"
-# sed -i "10i $SYM_CMD" $LINKER #thanks stackoverflow, but i still dont fully understand ;-;
+
